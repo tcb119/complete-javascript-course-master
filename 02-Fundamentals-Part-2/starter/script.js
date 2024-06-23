1bff9578f92d8ce576ac2064ea41818d7c658352
Caleb <kai_sunny@icloud.com>
2024-06-23 19:37:25 +0800

    調整eslint 參數設定檔設置
    目前是採用全域(針對整套專案資料夾)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,6 +8,8 @@ logger();
 logger();
 logger();
 
+
+
 function fruitProcessor(apples, oranges) {
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 export default [
-  {
-    languageOptions: { globals: globals.browser },
-    rules: {
-      'semi': 1,                     // 結尾分號確認 [警告]
-      'prefer-const': 'off',         // 強制設定無改變的變數要用const宣告 [關閉]
-      'quotes': ['error', 'single'], // 字串的定義符號 [錯誤，單引號]
-    }
-  },
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
-  {
-    files: ["complete-javascript-course-master/**/*.js", "complete-javascript-course-master/**/*.ts"],  // 指定文件?型
-  }
 ];
