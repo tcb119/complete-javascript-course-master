f4e64ff8999c2be8265643ac84befac969a2a9bf
Caleb <kai_sunny@icloud.com>
2024-07-01 11:28:04 +0800

    03-prettierrc test
    還沒設定跟Eslint綁定

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,30 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// 'use strict';
 
+
+// const x = '23';
+// if (x === 23) console.log(23);
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log(x);
+
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
