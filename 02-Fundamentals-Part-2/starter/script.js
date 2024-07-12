21daa693a412031012f4e874d6739ab7f2921ef2
Caleb <kai_sunny@icloud.com>
2024-07-13 00:55:30 +0800

    JS一些不明不白 重點釐清 vol.2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,10 @@
+/**
+ * use strict 嚴格模式在運行時除錯
+ * Eslint在靜態模式(即編寫過程中) 可依風格制定coding規範，或是當前編寫code除錯
+ */
+
 'use strict';
+
 
 // // Functions
 // function logger() {
@@ -11,83 +17,121 @@
 // logger();
 
 
+/**
+ * return 語句的作用是終止函數的執行，並返回一個值。
+ * 沒有 return 語句的函數將默認返回 undefined。
+ */
 
 // function fruitProcessor(apples, oranges) {
 //   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-//   return juice;
+//   return juice;   // 返回計算結果
 // }
+
+/**
+ * 經常宣告一個const來儲存值 
+ */
 
 // const appleJuice = fruitProcessor(5, 0);
 // console.log(appleJuice);
 
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
+// console.log(fruitProcessor(2, 4)); // 只負責輸出，不負責紀錄值。對比上面用了const的重要性
 
-// const num = Number('23');
-
-///////////////////////////////////////
-// Function Declarations vs. Expressions
+// ///////////////////////////////////////
+// // Function Declarations vs. Expressions
 
 // // Function declaration 函式宣告
-// function calcAge1(birthYeah) {
-//   return 2037 - birthYeah;
-// }
-// const age1 = calcAge1(1991);
 
-// console.log(calcAge1);
-// console.log('calcAge1 沒有宣告一個變數去接，就會顯示整個function。因為未定義前都叫呼叫？');
+/**
+ * 	1.	使用 function 關鍵字來定義函式。
+ *  2.	函式名稱是必需的。
+ *  3.	在程式碼執行之前會被提升（Hoisted），因此可以在宣告之前呼叫。
+ * 
+ *  但Function declaration 可以提前拉到開頭。
+ *  我們稱之為Hoisting
+ */
+const age1 = calcAge1(1991);
 
-// const calcAge = function calcAge1(birthYeah) {
-//   return 2037 - birthYeah;
-// }
-// console.log(calcAge);
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
 
-// // Function expression 函式表達式
-// const calcAge2 = function (birthYeah) {
-//   return 2037 - birthYeah;
-// }
-// const age2 = calcAge2(1991);
 
-// console.log(age1, age2);
+// Function expression 函式表達式
+const calcAge2 = function calcAge1(birthYeah) {
+  return 2037 - birthYeah;             //會直接把值存到 calcAge
+}
+const age2 = calcAge2(1991);
 
-// console.log(multiply(4, 5)); // 這會輸出 20
+console.log(age1, age2);
 
-// function multiply(a, b) {
-//   return a * b;
-// }
+/**
+ * Function 不是type 也是一種value
+ * 所以可以用一個「變數」儲存起來
+ * 
+ * 在 JavaScript（JS）中，類型（type）和值（value）比變數（variable）本身更為重要，
+ * 這背後有幾個原因，與 JavaScript 的設計哲學和運作方式密切相關。
+ * 
+ * 1. 是動態類型語言，這意味著變數在宣告時不需要指定其類型，類型是在程式執行期間根據賦予的值來確定的。
+ * 
+ * 2. 類型和值決定操作行為（操作符行為依賴於類型）
+ console.log(1 + "1");    // "11" (字串連接)
+ console.log(1 + 1);      // 2 (數字相加)
+ * 
+ * 3. 類型轉換和隱式轉換 自動類型轉換（Type Coercion）
+* console.log("5" - 1); // 4 (字串 "5" 被轉換為數字 5)
+ console.log("5" + 1); // "51" (數字 1 被轉換為字串 "1")
+ * 
+ * 4. 值的參考和複製
+let a = 10;
+let b = a;
+b = 20;
+console.log(a); // 10 (原始值是值的拷貝)
 
-// // Arrow function
+let obj1 = { name: "John" };
+let obj2 = obj1;
+obj2.name = "Jane";
+console.log(obj1.name); // "Jane" (引用值是參考)
+ */
+
+
+// ///////////////////////////////////////
+// // Arrow function （亦是一種 Function expression 函式表達式）
+/**
+ * 但要注意：Arrow function 拿不到this 這個關鍵字
+ * 或者該說無法使用
+ */
 // const calcAge3 = birthYeah => 2037 - birthYeah;
-// console.log(calcAge3);
 // const age3 = calcAge3(1991);
 // console.log(age3);
 
 // const yearUnitlRetirement = (birthYeah, firstName) => {
 //   const age4 = 2037 - birthYeah;
 //   const retirement = 65 - age4;
+//   // return retirement;
 //   return `${firstName} retires in ${retirement} years`
 // }
 
 // console.log(yearUnitlRetirement(1991, 'Jonas'));
 // console.log(yearUnitlRetirement(1980, 'Bob'));
 
-// ///////////////////////////////////////
-// // Functions Calling Other Functions
+///////////////////////////////////////
+// Functions Calling Other Functions
 // function cutFruitPieces(fruit) {
 //   return fruit * 4;
 // }
 
-// function fruitProcessor(apples, oranges) {
+// function fruitProcessor1(apples, oranges) {
 //   const applePieces = cutFruitPieces(apples);
 //   const orangePieces = cutFruitPieces(oranges);
-
 //   const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
 //   return juice;
 // }
-// console.log(fruitProcessor(2, 3));
+// console.log(fruitProcessor1(2, 3));
 
-///////////////////////////////////////
-// Reviewing Functions
+// /////////////////////////////////////
+// // Reviewing Functions
 // const calcAge = function (birthYeah) {
 //   return 2037 - birthYeah;
 // }
@@ -97,7 +141,7 @@
 //   const retirement = 65 - age;
 
 //   if (retirement > 0) {
-//     console.log(`${firstName} retires in ${retirement} years`);
+//     console.log(`${firstName} retires in ${retirement} years`); // 如果把這行，放在return後。程式立刻終止，不顯示以下。
 //     return retirement;
 //   } else {
 //     console.log(`${firstName} has already retired 🎉`);
@@ -108,11 +152,11 @@
 // console.log(yearsUntilRetirement(1991, 'Jonas'));
 // console.log(yearsUntilRetirement(1950, 'Mike'));
 
-// const calcAverage = (a, b, c) => (a + b + c) / 3;
-// console.log(calcAverage);
+// const calcAverage0 = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage0(3, 4, 5));
 
-// const scoreDolphins = calcAverage(44, 23, 71);
-// const scoreKoalas = calcAverage(65, 54, 49);
+// let scoreDolphins = calcAverage0(44, 23, 71);
+// let scoreKoalas = calcAverage0(65, 54, 49);
 // console.log(`scoreDolphins :${scoreDolphins} , scoreKoalas :${scoreKoalas}`);
 
 // const checkWinner = function (avgDolphins, avgKoalas) {
@@ -127,138 +171,279 @@
 // checkWinner(scoreDolphins, scoreKoalas);
 
 // checkWinner(576, 111);
+
+// Test 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+
 ///////////////////////////////////////
 // Introduction to Arrays
-// const friend1 = 'Michael';
-// const friend2 = 'Steven';
-// const friend3 = 'Peter';
 
-// const friends = ['Michael', 'Steven', 'Peter'];
+// Add elements
+// const friends = ['Michael','Steven', 'Peter'];
+// const newLength = friends.push('Jay');
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift('John'); // 這是添加在開頭
 // console.log(friends);
 
-// const years = new Array(1991, 1984, 2008, 2020);
-
-// console.log(friends[0]);
-// console.log(friends[2]);
-
-// console.log(friends.length);
-// console.log(friends[friends.length - 1]);
-
-// friends[2] = 'Jay';
+// // Remove elements
+// friends.pop(); // Remove last
+// const popped = friends.pop();
+// console.log(popped);   //peter
 // console.log(friends);
-// // friends = ['Bob', 'Alice']
 
-// const firstName = 'Jonas';
-// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-// console.log(jonas);
-// console.log(jonas.length);
+// friends.shift(); // Remove first
+// console.log(friends);
 
-// Exercise
-// const calcAge = function (birthYeah) {
-//   return 2037 - birthYeah;
-// }
-// const years = [1990, 1967, 2002, 2010, 2018];
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
 
-// const age1 = calcAge(years[0]);
-// const age2 = calcAge(years[1]);
-// const age3 = calcAge(years[years.length - 1]);
-// console.log(age1, age2, age3);
+// friends.push(23);
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+// console.log(friends.includes(23));
 
-// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-// console.log(ages);
-
-// const jonas = {
-//   firstName: 'Jonas',
-//   lastName: 'Schmedtmann',
-//   birthYeah: 2037 - 1991,
-//   job: 'teacher',
-//   friends: ['Michael', 'Peter', 'Steven'],
-//   hasDrivesLicensense: true,
-
-//   calcAge: function () {
-//     this.age = 2037 - this.birthYeah;
-//     return this.age;
-//   },
-
-//   getSummary: function () {
-//     return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDrivesLicensense ? 'a' : 'no'} driver's license.`
-//   }
-// };
-
-// console.log(jonas);
-
-// console.log(jonas.calcAge());
-// console.log(jonas.calcAge());
-// console.log(jonas.calcAge());
-// console.log(jonas.calcAge());
-
-// const mark = {
-//   fullName: 'Mark Miller',
-//   mass: 78,
-//   height: 1.69,
-//   calcBMI: function () {
-//     this.bmi = this.mass / this.height ** 2;
-//     return this.bmi;
-//   }
-// };
-
-// const john = {
-//   fullName: 'John Smith',
-//   mass: 92,
-//   height: 1.95,
-//   calcBMI: function () {
-//     this.bmi = this.mass / this.height ** 2;
-//     return this.bmi;
-//   }
-// };
-
-// mark.calcBMI();
-// john.calcBMI();
-
-// console.log(mark.bmi, john.bmi)
-
-// if (mark.bmi > john.bmi) {
-//   console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
-// } else if (john.bmi > mark.bmi) {
-//   console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+// if(friends.includes('Peter')) {
+//   console.log('You have a friend called Steven');
 // }
 
-
-// for loop keeps running while condition is TRUE
-// for (let rep = 1; rep <= 30; rep++) {
-//   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
-// }
 
 // Introduction to Objects
 
-/* i-- */
+/**
+ * 創造object屬性有很多方法
+ * 但使用{} 是最簡單的，一般稱作為：literal Syntax (物件文字語法)
+ */
 
 
-/* While迴圈 */
+/**
+ * Array 有順序，所以要根據索引值取出
+ * Object 是根據名字，應對更多 非結構化數據(unstructured data)
+ */
 
-// for (let rep = 1; rep <= 10; rep++) {
-//   console.log(`Lifting weights repetition ${rep} 🏋🏻‍♂️`);
-// }
+const jonasArray = [
+  'Jonas',
+  'Schmedtamnn',
+  2037 - 1991,
+  'teacher',
+  ['Michael','Steven', 'Steven']
+];
 
-// let rep = 1;
-// while (rep <= 10) {
-//   console.log(`Lifting weights repetition ${rep} 🏋🏻‍♂️`);
-//   rep++;
-// }
+  const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtamnn',
+    age: 2037 - 1991,
+    birthYeah: 1991,
+    job: 'teacher',
+    friends: ['Michael','Steven', 'Steven'],
+    hasDriversLicnese: true,
+
+     /**
+      * function attacked to an object called mathod
+      * 在這邊就是function value
+      */ 
+
+     /**
+       * 無法這樣寫入object裡 : 
+         function calcAge (birthYeah) {
+        return 2037 - birthYeah;
+        }
+       */
+
+      /**
+       * 但如果要避免輸入錯誤的址，
+       * 可以把這個
+         calcAge: function(birthYeah) {
+            return 2037 - birthYeah;
+         } 
+       * 改寫成以下：
+       */
+
+         calcAge: function() {
+          console.log(this);   // jonas calling this object. so it's pointed to jonas's property
+          return 2037 - this.birthYeah; // 有個好處是不會因object被更名，其他東西也要跟著異動
+       },
+
+       calcAge2: function() {
+        this.age = 2037 - this.birthYeah;   
+        return this.age;    // reuse 且不用怕寫錯代碼的好方式
+      },
+
+      getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+      }
+  };
+
+  console.log(jonas.calcAge());
+  console.log(jonas['calcAge']);
+  console.log(jonas.age);
+
+/**
+ * //Challenge
+ * "Jonas is a 46-year old teacher, and he has a driver's license"
+ */
+
+console.log(jonas.getSummary());
+
+/**
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+*/
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+
+
+/**
+ * bracket用法 可以替換成任意我們喜歡的詞語
+ */
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey)  //無法這樣寫
+
+/**
+ * 利用bracket 配合 輸入事件
+ * 呼叫事件觸發
+ */
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+/**
+ * 如何使用dot 與 bracket 爲object添加新屬性
+ */
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+/**
+ * 值得注意的是：
+ * dot(呼叫)的優先順序 > [ ]
+ */
+console.log(`${jonas.firstName} 
+  has ${jonas.friends.length} friends, 
+  and his best friend is called ${jonas.friends[0]}`);
+
+///////////////////////////////////////
+// Iteration: The for Loop
+
+
+// console.log('Lifting weights repetition 1 🏋️‍♀️');
+// console.log('Lifting weights repetition 2 🏋️‍♀️');
+// console.log('Lifting weights repetition 3 🏋️‍♀️');
+// console.log('Lifting weights repetition 4 🏋️‍♀️');
+// console.log('Lifting weights repetition 5 🏋️‍♀️');
+// console.log('Lifting weights repetition 6 🏋️‍♀️');
+// console.log('Lifting weights repetition 7 🏋️‍♀️');
+// console.log('Lifting weights repetition 8 🏋️‍♀️');
+// console.log('Lifting weights repetition 9 🏋️‍♀️');
+// console.log('Lifting weights repetition 10 🏋️‍♀️');
+
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋🏻‍♂️`);
+}
+
+const jonas2 = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+
+for (let i = 0; i < jonas2.length; i++) {
+  console.log(jonas2[i] , typeof jonas2[i]);
+
+  types.push(typeof jonas2[i]);
+}
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0 ; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and berak
+console.log('--- ONLY STRING ---')
+for (let i = 0; i < jonas2.length; i++) {
+  if(typeof jonas2[i] !== 'string') continue;
+
+  console.log(jonas2[i] , typeof jonas2[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas2.length; i++) {
+  if(typeof jonas2[i] !== 'number') break;
+
+  console.log(jonas2[i] , typeof jonas2[i]);
+}
+
+for (let i = jonas2.length -1; i >=0; i--) {
+  console.log(i, jonas2[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+
+for (let rep = 1; rep <6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
+}
+
+// while loop
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weights repetition ${rep} 🏋🏻‍♂️`);
+  rep++;
+}
 
 /*
 Math.trunc 是去掉小數值
 Math.random() 會出現0-1之間得數字
 */
 
-// let dice = Math.trunc(Math.random() * 6) + 1;
-// console.log(dice);
+let dice = Math.trunc(Math.random() * 6) + 1;
 
-// while (dice !== 6) {
-//   console.log(`You rolled a ${dice}`);
-//   dice = Math.trunc(Math.random() * 6) + 1;
-//   if (dice === 6) console.log('Loop is about to end...');
-// }
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
+
+/**
+ * 結論：
+ * while 迴圈用在 不需要計次，且不知道次數的時候
+ * for 迴圈則是明確知道會跑幾次，有個明確次數
+ */
 
 /* for迴圈 與槽狀使用 */
 
